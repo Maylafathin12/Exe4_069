3066dd03af1da74832fc88e5618064d6065f18de
TA1069 <maylafaat@gmail.com>
2022-12-07 16:53:43 +0700

    add case 2

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -80,7 +80,19 @@ namespace Exercise4
                 char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
                 switch (ch)
                 {
-                    case'1'
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
             }
         }
