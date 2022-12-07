582dad103bc922eadb4ef27819cab325647b3823
TA1069 <maylafaat@gmail.com>
2022-12-07 15:48:59 +0700

    membuat class program

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Exercise4
 {
+    //membuat class program
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        } 
+        //menggunakan array
+        int[] arr = new int[87];
+        int top;
+
     }
 }
